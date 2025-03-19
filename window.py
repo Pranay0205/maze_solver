7331a2b5f89fde4daba16e1e1c84ff303e95657a
Pranay Ghuge <35730341+Pranay0205@users.noreply.github.com>
2025-03-19 15:41:06 -0400

    Add basic window and canvas setup for Maze Solver

---
+++ window.py
@@ -0,0 +1,35 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.canvas = Canvas(self.__root, bg="white",
+                             width=self.width, height=self.height)
+        self.canvas.pack(fill=BOTH, expand=True)
+        self.is_running = True
+
+    def redraw(self):
+        self.__root.update()
+
+    def wait_for_close(self):
+        while self.is_running:
+            self.redraw()
+
+    def close(self):
+        self.is_running = False
+        self.__root.destroy()
+
+
+def main():
+    print("Initializing The Maze...")
+    win = Window(800, 600)
+    win.wait_for_close()
+
+
+if __name__ == "__main__":
+    main()

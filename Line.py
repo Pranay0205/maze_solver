b6e9702afc64876a887f1a9f2f8532c568539628
Pranay Ghuge <35730341+Pranay0205@users.noreply.github.com>
2025-03-19 16:15:11 -0400

    Add Point and Line for drawing

---
+++ Line.py
@@ -0,0 +1,10 @@
+class Line:
+    def __init__(self, start_point, end_point):
+        self.x1 = start_point.x
+        self.y1 = start_point.y
+        self.x2 = end_point.x
+        self.y2 = end_point.y
+
+    def draw(self, canvas, fill_color: str):
+        canvas.create_line(self.x1, self.y1, self.x2,
+                           self.y2, fill=fill_color, width=2)
--- window.py
+++ window.py
@@ -1,5 +1,8 @@
 from tkinter import Tk, BOTH, Canvas
 
+from Line import Line
+from Point import Point
+
 
 class Window:
     def __init__(self, width, height):
@@ -24,10 +27,27 @@ class Window:
         self.is_running = False
         self.__root.destroy()
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
 
 def main():
     print("Initializing The Maze...")
     win = Window(800, 600)
+    point_a = Point(10, 10)
+    point_b = Point(10, 90)
+    point_c = Point(45, 10)
+    point_d = Point(10, 45)
+    point_e = Point(45, 45)
+
+    line = Line(point_a, point_b)
+    win.draw_line(line, "red")
+    line2 = Line(point_a, point_c)
+    win.draw_line(line2, "red")
+    line3 = Line(point_c, point_e)
+    win.draw_line(line3, "red")
+    line4 = Line(point_e, point_d)
+    win.draw_line(line4, "red")
     win.wait_for_close()
 
 
